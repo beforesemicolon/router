1368757cd259c82dde0405a765fc6666e879584c
Elson Correia <design@elsoncorreia.com>
2024-11-28 18:32:59 -0500

    update markup@next

--- src/utils/is-object-literal.ts
+++ src/utils/is-object-literal.ts
@@ -1,8 +1,7 @@
 export const isObjectLiteral = (val: unknown) => {
-    if (typeof val !== 'object' || val === null) {
-        return false
-    }
-
-    // Check if the object's constructor is Object
-    return Object.getPrototypeOf(val) === Object.prototype
+    return (
+        val &&
+        typeof val === 'object' &&
+        Object.getPrototypeOf(val) === Object.prototype
+    )
 }
